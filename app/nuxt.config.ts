65e7bb325fb65318d8dc3e5b1fec16fd1baeecb8
Lfu001 <82690385+Lfu001@users.noreply.github.com>
2025-03-09 20:48:15 +0900

    chore(app): add tailwind dependency (#16)

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,8 +1,16 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/storybook', '@nuxt/eslint'],
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-11-01',
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   eslint: {
     config: {
       stylistic: true,
