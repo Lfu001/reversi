7d0103f84b57ab44406a5c2f89fd03eae0d62b18
Lfu001 <goodspeed913@gmail.com>
2025-05-18 18:26:25 +0900

    feat(app): add outlined variant of button component

--- app/components/Button/Button.vue
+++ app/components/Button/Button.vue
@@ -1,10 +1,20 @@
 <template>
   <button
     :class="[
-      'rounded-full px-4 py-2 font-bold text-white opacity-80 transition-colors duration-150 select-none focus:outline-none',
-      props.disabled
-        ? 'bg-gray-400'
-        : 'cursor-pointer bg-indigo-500 hover:bg-indigo-600 focus:ring-0 active:bg-indigo-700',
+      'rounded-full px-4 py-2 font-bold transition-colors duration-150 select-none focus:outline-none',
+      props.variant === 'outlined'
+        ? [
+          'border-2',
+          props.disabled
+            ? 'border-gray-400 text-gray-400'
+            : 'border-indigo-500 text-indigo-500 hover:bg-indigo-50 active:bg-indigo-100',
+        ]
+        : [
+          'text-white',
+          props.disabled
+            ? 'bg-gray-400'
+            : 'cursor-pointer bg-indigo-500 hover:bg-indigo-600 focus:ring-0 active:bg-indigo-700',
+        ],
     ]"
     @click="handleClick"
   >
@@ -13,21 +23,28 @@
 </template>
 
 <script setup lang="ts">
-/**
- * Props for the Button component.
- * @prop {string} label - The text to display on the button.
- * @prop {boolean} [disabled] - Determines if the button is disabled.
- */
-const props = defineProps<{
+const props = withDefaults(defineProps<{
+  /**
+   * The text to display on the button.
+   */
   label: string
+  /**
+   * Whether the button is disabled.
+   */
   disabled?: boolean
-}>()
+  /**
+   * The style of the button.
+   */
+  variant?: 'fill' | 'outlined'
+}>(), {
+  disabled: false,
+  variant: 'fill',
+})
 
-/**
- * Emits events for the Button component.
- * @event click - Emitted when the button is clicked, if not disabled.
- */
 const emit = defineEmits<{
+  /**
+   * Emitted when the button is clicked, if not disabled.
+   */
   (e: 'click'): void
 }>()
 
