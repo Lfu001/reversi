4d14b20c9ef7487e864defced4b860e582191254
kenken <tsunapiman@gmail.com>
2025-06-11 19:02:19 +0900

    fix(app): fix border thickness

--- app/components/BoardContainer/BoardContainer.vue
+++ app/components/BoardContainer/BoardContainer.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bg-[#4a3222] p-8 rounded-[15px] shadow-lg flex justify-center items-center w-[380px] h-[380px] select-none">
     <div class="bg-[radial-gradient(#50aa50,#2d642d)] p-2.5 rounded-[10px]">
-      <div class="grid grid-cols-8 grid-rows-8 border border-black w-[320px] h-[320px]">
+      <div class="grid grid-cols-8 grid-rows-8 border-b-2 border-r-2 border-black w-[320px] h-[320px]">
         <div
           v-for="(c, index) in board"
           :key="index"
-          class="w-10 h-10 flex items-center justify-center relative border border-black"
+          class="w-10 h-10 flex items-center justify-center relative border-t-2 border-l-2 border-black"
           :class="{ 'before:content-[\'\'] before:absolute before:w-[6px] before:h-[6px] before:rounded-full before:bg-black before:top-[-1px] before:left-[-1px] before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:opacity-100': isDotPosition(index) }"
         >
           <DiskComponent
