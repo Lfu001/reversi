40e93b15f1822533fe6446358be2ed1a4339dc6b
imaPnu2 <tsunapiman@gmail.com>
2025-06-11 19:31:26 +0900

    feat(app) Create board UI (#14)
    * feat(app): add board UI
    
    * feat(app): change board shape
    
    * feat(app): add board shadow
    
    * feat(app): disable selection for each element of the board
    
    * fix(app): untabify
    
    * fix(app): rename board class to board-container
    
    * fix(app): rename Board component to BoardContainer
    
    * fix(app): fix eslint error
    
    * feat(app): adjust the outer border thickness
    
    * feat(app): change board color
    
    * feat(app): add DiskComponent
    
    * feat(app): add disk component
    
    * fix(app): fix board compornent
    
    * fix(app): delete unnecessary file
    
    * fix(app): renames variable
    
    * fix(app): add board story
    
    * fix(app): fix board component
    
    * Revert "fix(app): delete unnecessary file"
    
    This reverts commit 072e8b73f54f89511d3fc63896a77a3065508e1c.
    
    * fix(app): delete unnecessary file
    
    * fix(app): delete unnecessary file
    
    * fix(app): fix app.vue changes
    
    * fix(app): add tailwind css
    
    * fix(app): add tailwind css
    
    * fix(app): fix border thickness

---
+++ app/types/Position.ts
@@ -0,0 +1,48 @@
+const Row = {
+  One: 0,
+  Two: 1,
+  Three: 2,
+  Four: 3,
+  Five: 4,
+  Six: 5,
+  Seven: 6,
+  Eight: 7,
+} as const
+
+type Row = typeof Row[keyof typeof Row]
+
+const Column = {
+  A: 0,
+  B: 1,
+  C: 2,
+  D: 3,
+  E: 4,
+  F: 5,
+  G: 6,
+  H: 7,
+} as const
+
+type Column = typeof Column[keyof typeof Column]
+
+class Position {
+  row: Row
+  column: Column
+
+  constructor(row: Row, column: Column) {
+    this.row = row
+    this.column = column
+  }
+
+  /**
+   * Creates a Position from a 0-indexed number.
+   * @param index - The index of the position. Must be in the range 0-63.
+   * @returns The Position corresponding to the given index, or null if the index is out of bounds.
+   */
+  static fromIndex(index: number) {
+    const row = Math.floor(index / 8)
+    const column = index % 8
+    return new Position(row as Row, column as Column)
+  }
+}
+
+export { Position }
