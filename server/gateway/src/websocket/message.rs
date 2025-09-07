5008946f921415f46ef83df64e0e3c93bd00c3d6
Lfu001 <goodspeed913@gmail.com>
2025-09-07 13:33:16 +0900

    docs(gateway): clarify the direction of each WsMessage

--- server/gateway/src/websocket/message.rs
+++ server/gateway/src/websocket/message.rs
@@ -4,16 +4,14 @@ use serde::{Deserialize, Serialize};
 /// A message that is sent over the WebSocket connection.
 #[derive(Serialize, Deserialize, Clone)]
 pub enum WsMessage {
-    /// A message sent to a client when another player joins the table.
+    /// **(Client <= Server)** Another player joins the table.
     Connected(String),
-    /// A message sent to a client when another player leaves the table.
+    /// **(Client <= Server)** Another player leaves the table.
     Disconnected(String),
-    /// A message sent from a client to the server to make a move.
+    /// **(Client => Server)** The player makes some reversi action.
     Step(Action),
-    /// A message sent from the server to all clients in the table
-    /// to update their game state.
+    /// **(Client <= Server)** The game state is updated.
     GameState(StateResponseMessage),
-    /// A message sent from the server to a client to signal an internal
-    /// server error.
+    /// **(Client <= Server)** Internal server error occurred.
     InternalServerError,
 }
