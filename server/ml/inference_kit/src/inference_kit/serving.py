07bd20d59fb07442a196a93ab069980423f9cfc0
Lfu001 <goodspeed913@gmail.com>
2025-09-28 21:35:22 +0900

    feat(ml): add health check endpoint

--- server/ml/inference_kit/src/inference_kit/serving.py
+++ server/ml/inference_kit/src/inference_kit/serving.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 
 from .api_model import PredictionOutput, TableState
 from .handler_service import HandlerService
 
 app = FastAPI()
 handler_service = HandlerService()
+
+
+@app.get("/health")
+def health():
+    return Response(status_code=status.HTTP_200_OK)
 
 
 @app.post("/invocations", response_model=PredictionOutput)
