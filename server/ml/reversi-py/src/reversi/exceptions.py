6b59800ab5bbefabce2329917768749426295fe1
Lfu001 <goodspeed913@gmail.com>
2025-10-12 18:53:47 +0900

    style(reversi-py): apply formatter

--- server/ml/reversi-py/src/reversi/exceptions.py
+++ server/ml/reversi-py/src/reversi/exceptions.py
@@ -1,7 +1,6 @@
 class ReversiError(Exception):
     """Base exception for all Reversi-related errors."""
-    pass
+
 
 class InvalidMoveError(ReversiError):
     """Raised when an invalid move is attempted."""
-    pass
