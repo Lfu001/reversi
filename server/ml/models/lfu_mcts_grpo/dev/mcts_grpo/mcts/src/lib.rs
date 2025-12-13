66660f86000ce0269ce9f5feb067ac252e571204
Lfu001 <goodspeed913@gmail.com>
2025-12-14 01:57:08 +0900

    perf(mcts-grpo): introduce jemallocator to make memory allocation efficient

--- server/ml/models/lfu_mcts_grpo/dev/mcts_grpo/mcts/src/lib.rs
+++ server/ml/models/lfu_mcts_grpo/dev/mcts_grpo/mcts/src/lib.rs
@@ -9,6 +9,12 @@ mod transposition_table;
 mod tree;
 
 use pyo3::prelude::*;
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 #[pymodule]
 fn _core(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
